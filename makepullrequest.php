2594ab1b3b51cea87b934c3f4ad254ce58598165
bcarbonnier <bcarbonnier@v20.fdsetu.infra.umontpellier.fr>
2018-04-12 08:50:11 +0200

    webhook

---
+++ makepullrequest.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+   <title>Webservice BDD Ter 16</title>
+</head>
+<body>
+<font color="blue">Webservice BDD Ter 16</font>
+
+<?php
+	echo exec("git pull");
+?>
+
+
+</body>
+</html>
