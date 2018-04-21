7fdc04d48486438aebf8a8b5cda252805a8e0881
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-04-21 14:42:49 +0200

    Test pdo

--- request.php
+++ request.php
@@ -20,7 +20,8 @@ try {
     $select = $db->prepare("SELECT * FROM :table WHERE id =:id");
     $table = urldecode($_GET['table']);
     $id = urldecode($_GET['id']);
-    $select->bindParam("ss",$table,$id);
+    $select->bindParam(':table',$table);
+    $select->bindParam(':id',$id);
     $select->execute();
 
     $result = $select->fetchAll();
