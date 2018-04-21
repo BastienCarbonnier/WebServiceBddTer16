2fa8c772918d369718ded03858cd74078c5569cf
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-04-21 14:35:54 +0200

    Test pdo

--- request.php
+++ request.php
@@ -18,8 +18,8 @@ try {
 
 
     $select = $db->prepare("SELECT * FROM :table WHERE id = :id_rel");
-    $table = "listerelation";//urldecode($_GET['table']);
-    $id = "4";//urldecode($_GET['id']);
+    $table = "‘listerelation‘’";//urldecode($_GET['table']);
+    $id = "‘4‘";//urldecode($_GET['id']);
     $select->bindParam(':table', $table);
     $select->bindParam(':id_rel', $id);
     $select->execute();
