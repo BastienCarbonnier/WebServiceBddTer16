bbb9e667c07da32a0fe1a081a839c8eba969c5d8
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-04-21 14:16:50 +0200

    Test pdo

--- request.php
+++ request.php
@@ -10,34 +10,26 @@ error_reporting(E_ALL | E_STRICT);
 
 include ("../../.sqlpass.php");
 
-$cmd = urldecode($_GET['cmd']);
-$champ = urldecode($_GET['champ']);
-
-
-$query = $cmd." ".$champ." FROM ".$from;
-
-if (isset($_GET['where'])){
-    $where = urldecode($_GET['where']);
-    $query .=" WHERE ".$where.";";
-}
-else{
-    $query .= ";";
-}
-
-echo "<p>".$query."</p>";
 try {
     $db = new PDO('mysql:host=localhost;dbname='.$user, $user, $mdp);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     echo '<result>';
 
+
     $select = $db->prepare("SELECT * FROM :table WHERE id = :id");
     $select->bindParam(':table', $table);
     $select->bindParam(':id', $id);
+
     $table = urldecode($_GET['table']);
     $id = urldecode($_GET['id']);
+
     $select->execute();
 
+    $result = $select->setFetchMode(PDO::FETCH_ASSOC);
+    foreach($select->fetchAll()) as $k=>$v) {
+        echo $v;
+    }
     echo '</result>';
     $db = null;
 } catch (PDOException $e) {
