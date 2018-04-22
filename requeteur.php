1786a7402f6f1f976a9c5c46451fdda690126526
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-04-22 19:12:11 +0200

    test webservice

--- requeteur.php
+++ requeteur.php
@@ -9,14 +9,14 @@
 error_reporting(E_ALL | E_STRICT);
 echo "Partie 1";
 
-include_once "fonction.php";
+
 echo "partie 2";
 include ("../../.sqlpass.php");
 echo "partie 3";
 try {
 	$db = new PDO('mysql:host=localhost;dbname='.$user, $user, $mdp);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+include_once "fonction.php";
 echo "partie 4";
 
 $rqt = urldecode($_GET["rqt"]);
