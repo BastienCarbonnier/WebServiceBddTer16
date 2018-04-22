3e66df64313e5625b2904e9e931b14e47f1614e9
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-04-22 19:05:49 +0200

    Rajout balise html

--- requeteur.php
+++ requeteur.php
@@ -7,16 +7,16 @@
 
 <?php
 error_reporting(E_ALL | E_STRICT);
-
+echo "Partie 1";
 include_once 'fonction.php';
-
+echo "partie 2";
 include ("../../.sqlpass.php");
-
+echo "partie 3";
 try {
 	$db = new PDO('mysql:host=localhost;dbname='.$user, $user, $mdp);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
+echo "partie 4";
 
 $rqt = urldecode($_GET["rqt"]);
 $table = strval(urldecode($_GET["from"]));
