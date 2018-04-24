4d01e10129cb97ddc51da76e195bc30bcb9b8c38
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-04-24 17:22:38 +0200

    rajout insert_rel

--- requeteur.php
+++ requeteur.php
@@ -136,7 +136,7 @@ switch($rqt){
         echo "<br/>";
         echo "<br/>";
         echo "<br/>";
-        echo $result[0]->rid;
+        echo $result[0]["rid"];
         /*
         $attributs = "n1,n2,t,user_id";
 
