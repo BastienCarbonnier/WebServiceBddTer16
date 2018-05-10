724c1500b1e015e2cec3e2621bcf0a892de2595f
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-05-10 16:41:14 +0200

    modif requete post

--- requeteur.php
+++ requeteur.php
@@ -266,9 +266,9 @@ switch($cmd){
         }
         break;
     case "insert_user":
-        $pseudo = strval(urldecode($_POST["pseudo"]));
-        $idBot = strval(urldecode($_POST["idBot"]));
-        $session = strval(urldecode($_POST["session"]));
+        $pseudo = strval($_POST["pseudo"]);
+        $idBot = strval($_POST["idBot"]);
+        $session = strval($_POST["session"]);
 
         $table = "user";
         $attributs = "pseudo,idBot,session";
