ecab10197096ea8efc2fd7158254738ee3ef6ede
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-05-17 13:58:50 +0200

    Rajout is relation exist

--- requeteur.php
+++ requeteur.php
@@ -284,8 +284,8 @@ switch($cmd){
         $n1 = strval(urldecode($_GET["n1"]));
         $n2 = strval(urldecode($_GET["n2"]));
         $t = strval(urldecode($_GET["t"]));
-
-            $fa = strval(urldecode($_GET["fa"]));
+        $pseudo = strval(urldecode($_GET["pseudo"]));
+        $fa = strval(urldecode($_GET["fa"]));
         updateLastFaFw($fa,$n1,$pseudo);
 
         if (is_relation_exist($n1,$n2,$t)){
